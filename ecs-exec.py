9f5e30da504b56621850a6ca7640f541f070b193
Momo <egyangel93@gmail.com>
2024-09-17 10:31:40 +0200

    subject: production

--- ecs-exec.py
+++ ecs-exec.py
@@ -38,9 +38,7 @@ def main():
     print("No tasks found in the selected cluster.")
     return
 
-  task_details = {}export AWS_ACCESS_KEY_ID="ASIA3FDUJBE4GKCWZ6ML"
-                   export AWS_SECRET_ACCESS_KEY="wKpRkwwIvOlEzAhftKkJc+MDXPbzW/T1joN7Q+ai"
-                   export AWS_SESSION_TOKEN="IQoJb3JpZ2luX2VjEDwaDGV1LWNlbnRyYWwtMSJIMEYCIQDtPKO7FtItaLmXfLuAep1/FY/88DLJSKvAhXlAB7iUVAIhAJJDiVV+iRMa7VROcXMvW8P+zZ8kkMkdI0xTtp762aARKtQDCBUQARoMNzY2ODk1NTI0MTUyIgxfk+/rB5P7bnKFuTkqsQNZaHH8In/uFQD04QZnFlWoteHSCrpzq5qMcgiEPxTKbQ+cipkWh+ng5rQs5Kv4JatOl8Jxe0YvcsyubbMle9Vz2HC7HROMi2szAqHUe/J0IyYi7BepvEvKI55DYy434FKAaPSpsPeSCyR295+udIvM7isLre69IZp3RpniLxT+vYtFWYF2UG+f7ZaEvP6wCq4Gi1SyXbnLbhs5hBvitz/yTP0RDHM3R1dFU7EtK9ThzwhXmeX4TtV+FD676zS9gJhFokQDGY5zI7TZdbqF0ovznYHcF8OhqH4vN8PDNBprKzQBYy0VUNeWEcqLWo3aoTiCbT0202bEyue3rde6vreSWsFfm4noB2MgUYNC38RVzr56joDdkL0CyAjotU12nUb1QCVLz2dafDqXHPOdF10I5IaLYTH+mGh5S6coITJl7LqaOXKkYkJeJGOoTBj4i5or6+8utHirpYmBIhTGw0PSr8A+5CIuJIieNE8mZClBadKUcVxve4vDOm/d2oU80yrVHR7YSXadEFqO/HfNIPUNtZWNfIpDb4lD4alk9KSU34ybawuU/CZrAzxkrF+QT3UrMMSP5LQGOqUB4xUXoREYREGclboK/iXOgUowQbPmaK5cntPOFfxl+N1bUrMAbVfHS4mc+CoAvPkoHp2YTi6cekUZ7w4EfRhGsgn0ZFO+rwKU2wItWnMrlwZUylecPqRGow9Zpjj3KV33LKOGctN718uyaaPivHOSBhNtJ3gIbpnyrocGdbdCMvPbDWYqHrJID0MjP5YwQNzc2ZvOFr9eS+p38RociJ0dXYJgUOqf"
+  task_details = {}
   for task in tasks:
     task_info = describe_task(selected_cluster, task)
     for container in task_info['containers']:
